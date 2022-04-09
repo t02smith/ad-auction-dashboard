fc65eb47cc4e41b094fc7c25bc6ac58b861b2e2f
Piriyan Karu <ik3g20@soton.ac.uk>
2022-04-09 19:20:02 +0100

    Added Basic Dashboard

--- app/src/main/java/ad/auction/dashboard/view/pages/CampaignPage.java
+++ app/src/main/java/ad/auction/dashboard/view/pages/CampaignPage.java
@@ -3,6 +3,7 @@ package ad.auction.dashboard.view.pages;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
@@ -27,6 +28,7 @@ import ad.auction.dashboard.view.Graph_Models.Graphs.LineChartModel;
 import ad.auction.dashboard.view.ui.Window;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
 /**
@@ -44,13 +46,16 @@ public class CampaignPage extends BasePage {
     private String campaignName;
 
     private LineChartModel graph;
+    private StackPane dashBoard = new StackPane();
     private final BorderPane screen = new BorderPane();
 
     private Metrics currentMetric = DEFAULT_METRIC;
 
     private final Button histogramToggle = new Button("Histogram");
+    private final Button dashButton = new Button("Dashboard");
     private boolean histogramActive = false;
     private boolean cumulative = false;
+    private boolean onDashboard = false;
 
     //Function to load a given metric
     @SuppressWarnings("unchecked")
@@ -151,12 +156,14 @@ public class CampaignPage extends BasePage {
         gc.strokeLine(buttonWidth * 0.25, buttonHeight * 0.5, buttonWidth * 0.5, buttonHeight * 0.725);
         gc.strokeLine(buttonWidth * 0.25, buttonHeight * 0.5, buttonWidth * 0.5, buttonHeight * 0.275);
 
+
         // Stack the drawn arrow on the button
         var pane = new StackPane();
         pane.getChildren().addAll(backButton, canvas);
 
         return pane;
     }
+
 
     private BorderPane title() {
         //Title background on top
@@ -176,12 +183,22 @@ public class CampaignPage extends BasePage {
 
         //Settings page button
         var editButton = new Button("Settings");
+        editButton.setPrefHeight(40);
+        editButton.setPrefWidth(150);
         BorderPane.setAlignment(editButton, Pos.CENTER);
         editButton.getStyleClass().add("buttonStyle");
         editButton.setOnMouseClicked((e) -> window.openEditPage(campaignName, window::startMenu));
 
+        //DashBoard toggle button
+        dashButton.setPrefHeight(40);
+        dashButton.setPrefWidth(150);
+        dashButton.getStyleClass().add("buttonStyle");
+
+
         //cumulative toggle
         var cumToggle = new Button("Cumulative");
+        cumToggle.setPrefHeight(40);
+        cumToggle.setPrefWidth(150);
         cumToggle.getStyleClass().add("buttonStyle");
         cumToggle.setOnAction(e -> {
             cumulative = !cumulative;
@@ -190,10 +207,13 @@ public class CampaignPage extends BasePage {
             loadMetric.accept(currentMetric);
         });
 
-        var right = new HBox(histogramToggle, cumToggle, editButton);
+        var right = new HBox(histogramToggle, cumToggle, dashButton, editButton);
+        BorderPane.setMargin(right, new Insets(0, 3 ,0, 0));
         right.setSpacing(25);
         right.setAlignment(Pos.CENTER);
 
+        histogramToggle.setPrefHeight(40);
+        histogramToggle.setPrefWidth(150);
         histogramToggle.setVisible(false);
         histogramToggle.getStyleClass().add("buttonStyle");
         title.setRight(right);
@@ -233,8 +253,66 @@ public class CampaignPage extends BasePage {
         } else {
             histogramToggle.setVisible(false);
         }
+
+        
+        dashButton.setText("Dashboard");
+        onDashboard = false;
+        dashButton.setOnAction(e -> {
+            if (onDashboard) {
+                logger.info("Changing to Linechart");
+                onDashboard = false;
+                dashButton.setText("Dashboard");
+                loadMetric.accept(currentMetric);
+            } else {
+                logger.info("Loading Dashboard");
+                onDashboard = true;
+                dashButton.setText("Graph");
+                this.screen.setCenter(this.dashBoard);
+                dashBoard.getChildren().add(buildDashBoard());
+
+            }
+        });
+
     }
 
+    private BorderPane buildDashBoard() {
+        var dashPane = new BorderPane();
+        dashPane.getStyleClass().setAll("bg-primary");
+
+        var dTitle = new Label("Dashboard");
+        dTitle.setAlignment(Pos.CENTER);
+        dTitle.getStyleClass().setAll("topTitle");
+
+        dashPane.setTop(dTitle);
+        BorderPane.setAlignment(dTitle, Pos.CENTER);
+
+        var metricBox = new VBox();
+        metricBox.setPadding(new Insets(5, 0, 0, 15));
+        metricBox.setSpacing(15);
+
+        for (Metrics m : Metrics.values()) {
+            var metLabel = new Label(m.getMetric().displayName());
+            metLabel.setStyle("-fx-font-size: 15px");
+            var overAll = this.controller.runCalculation(m, MetricFunction.OVERALL);
+            overAll.forEach((key, value) -> {
+                try {
+                    var metString = String.format(m.getMetric().displayName() + " : %s", value.get());
+                    metLabel.setText(metString);
+                } catch (Exception ignored) {}
+            });
+
+            metricBox.getChildren().add(metLabel);
+            
+
+        }
+
+        dashPane.setCenter(metricBox);
+        BorderPane.setAlignment(metricBox, Pos.CENTER);
+
+        return dashPane;
+
+    }
+    
     private ButtonList<String> campaignList() {
         var cList = controller.getCampaigns().stream()
                 .map(Campaign.CampaignData::name)
--- app/src/main/java/ad/auction/dashboard/view/pages/MainMenuPage.java
+++ app/src/main/java/ad/auction/dashboard/view/pages/MainMenuPage.java
@@ -114,7 +114,6 @@ public class MainMenuPage extends BasePage {
                     return null;
                 }
             };
-
             task.setOnSucceeded((ee) -> window.openCampaignPage(name));
 
             new Thread(task).start();
@@ -128,4 +127,5 @@ public class MainMenuPage extends BasePage {
     private void settings() {
 
     }
+
 }
